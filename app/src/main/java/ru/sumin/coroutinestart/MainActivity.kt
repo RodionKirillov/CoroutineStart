4c00da37f5be8f71ee06e21a2fe71c2f4091fa2f
RodionKirillov <rodion.44454@gmail.com>
2024-06-05 16:36:26 +0300

    add Coroutine (Внутреннее устройсво)

--- app/src/main/java/ru/sumin/coroutinestart/MainActivity.kt
+++ app/src/main/java/ru/sumin/coroutinestart/MainActivity.kt
@@ -25,9 +25,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.buttonLoad.setOnClickListener {
 
-            lifecycleScope.launch {
-                loadData()
-            }
+//            lifecycleScope.launch {
+//                loadData()
+//            }
+            loadWithoutCoroutine()
 
         }
     }
@@ -58,4 +59,51 @@ class MainActivity : AppCompatActivity() {
         delay(5000)
         return 17
     }
+
+    private fun loadWithoutCoroutine(step: Int = 0, obj: Any? = null) {
+        when (step) {
+            0 -> {
+                binding.progress.isVisible = true
+                binding.buttonLoad.isEnabled = false
+                loadCityWithoutCoroutine {
+                    loadWithoutCoroutine(1, it)
+                }
+            }
+
+            1 -> {
+                val city = obj as String
+                binding.tvLocation.text = city
+                loadTemperatureWithoutCoroutine(city) {
+                    loadWithoutCoroutine(2, it)
+                }
+            }
+
+            2 -> {
+                val temp = obj as Int
+                binding.tvTemperature.text = temp.toString()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
+        }
+    }
+
+    private fun loadTemperatureWithoutCoroutine(city: String, callback: (Int) -> Unit) {
+        Toast.makeText(
+            this,
+            getString(R.string.loading_temperature_toast, city),
+            Toast.LENGTH_SHORT
+        ).show()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            callback.invoke(17)
+        }, 5000)
+    }
+
+
+    private fun loadCityWithoutCoroutine(callback: (String) -> Unit) {
+        Handler(Looper.getMainLooper()).postDelayed({
+            callback.invoke("Moscow")
+        }, 5000)
+    }
 }
+
