e9a5c193a684ff602b08a7bfb7a29a474670de0d
RodionKirillov <rodion.44454@gmail.com>
2024-07-20 15:39:11 +0300

    Exception Handling

---
+++ app/src/main/java/ru/sumin/coroutinestart/MainViewModel.kt
@@ -0,0 +1,48 @@
+package ru.sumin.coroutinestart
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
+
+class MainViewModel : ViewModel() {
+
+    private val parentJob = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob)
+
+    fun method() {
+        val childJob1 = coroutineScope.launch {
+            delay(3000)
+            Log.d(LOG_TAG, "first coroutine finished")
+        }
+
+        val childJob2 = coroutineScope.launch {
+            delay(2000)
+            Log.d(LOG_TAG, "second coroutine finished")
+        }
+
+        thread {
+            Thread.sleep(1000)
+            parentJob.cancel()
+            Log.d(LOG_TAG, "Parent Job isActive: ${parentJob.isActive}")
+        }
+
+        Log.d(LOG_TAG, parentJob.children.contains(childJob1).toString())
+        Log.d(LOG_TAG, parentJob.children.contains(childJob2).toString())
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
+    }
+
+    companion object {
+
+        private const val LOG_TAG = "LOG_TAG"
+    }
+}
